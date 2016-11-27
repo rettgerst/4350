69d8650e15d133cd2b402f1dd0a7ef05b68b033d
rett gerst <ggerst1@student.gsu.edu>
2016-11-26 19:11:15 -0600

    add script to insert test data into database

---
+++ insert.sql
@@ -0,0 +1,11 @@
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`) 
+values ('Cook Out Northside', 33.785686, -84.4098867, 'American', TRUE, FALSE, b'1111111', '10:30', '4:00');
+
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`) 
+values ('Cook Out Ponce', 33.774611, -84.3713277, 'American', TRUE, FALSE, b'1111111', '10:30', '4:00');
+
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`) 
+values ('Cook Out Moreland', 33.7431083, -84.3669767, 'American', TRUE, FALSE, b'1111111', '10:30', '4:00');
+
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`) 
+values ('Sidebar', 33.757033, -84.3921116, 'American', TRUE, FALSE, b'1111111', '11:00', '24:00');
