b783f0f688d08819a64586f95ca30a14d458b0db
rett gerst <ggerst1@student.gsu.edu>
2016-11-26 19:10:01 -0600

    search functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 var session = require('express-session');
 var passport = require('passport');
 var bodyParser = require('body-parser');
+var Forecast = require('forecast');
 var config;
 if (!fs.existsSync('config.js')) {
 	console.log('please create configuration file config.js\nsee config-example.js for examples');
@@ -11,6 +12,7 @@ if (!fs.existsSync('config.js')) {
 	config = require('./config.js');
 }
 
+var forecast = new Forecast(config.forecast);
 var mysql = require('mysql');
 var app = express();
 app.use(session({
@@ -26,10 +28,18 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+
 app.get('/', function (req, res) {
-	res.render('index', {
-		user: req.user
-	});
+	if (!req.user) {
+		res.render('register', {
+			user: req.user
+		});
+	} else {
+		res.render('index', {
+			user: req.user
+		});
+	}
 });
 
 app.post('/api/register', function (req, res) {
@@ -41,6 +51,41 @@ app.post('/api/register', function (req, res) {
 
 app.post('/api/login', passport.authenticate('local-login'), function (req, res) {
 	return res.redirect('/');
+});
+
+app.post('/api/searchRestaurants', function (req, res) {
+	res.redirect('/search/restaurants/' + req.body.query);
+});
+
+app.get('/search/restaurants/:query', function (req, res) {
+	db.query('select * from restaurants where name like \'%' + req.params.query + '%\'', function (err, rows) {
+		res.render('searchresults', {
+			query: req.params.query,
+			rows: rows
+		});
+	});
+});
+
+app.get('/restaurant/:id', function (req, res) {
+	var query = 'select * from restaurants where id=' + req.params.id + ' order by id asc limit 1;';
+	async.waterfall([
+		(cb) => { db.query(query, cb); },
+		(rows, fields, cb) => {
+			var rest = rows[0];
+			forecast.get([rest.lat, rest.long], (err, weather) => {
+				if (err) return cb(err);
+				else return cb(null, {
+					restaurant: rest,
+					weather: weather
+				});
+			});
+		}
+	], (err, results) => {
+		if (err) {
+			console.error(err);
+			return res.sendStatus(500);
+		} else res.render('restaurant', results);
+	});
 });
 
 var db = mysql.createConnection(config.mysql);
