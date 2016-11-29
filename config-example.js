32b0024a31f6bdaa9d8ed01b1353d7c3b1b1d04a
rett gerst <tricagamma@gmail.com>
2016-11-29 14:38:13 -0500

    update config-example.js

--- config-example.js
+++ config-example.js
@@ -1,8 +1,26 @@
 module.exports = {
+	host: 'localhost:3000',
 	mysql: {
-		host     : '',
-		user     : '',
-		password : '',
-		database : ''
-	}
+		host     : 'localhost',
+		user     : 'root',
+		password : 'root',
+		database : 'patio'
+	},
+	server: {
+		port: 3000
+	},
+	session_store: '', // session store for express-session
+	store_opts: {}, // consult your chosen store for options
+	forecast: {
+		service: 'darksky',
+		key: '', // darksky api key
+		units: 'fahrenheit',
+		cache: true,
+		ttl: {
+			minutes: 27,
+			seconds: 45
+		}
+	},
+	transport: '', // transport module for use with nodemailer
+	transport_options: { } // consult your chosen transport for options
 };
