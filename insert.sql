d21cffe7821205b2bff8d9fd6d2d7be761e754a7
rett gerst <tricagamma@gmail.com>
2016-11-29 01:31:03 -0500

    fix quotes

--- insert.sql
+++ insert.sql
@@ -1,17 +1,17 @@
-insert into restaurants ('name', 'lat', 'long', 'cuisine', 'indoor','outdoor', 'days', 'open', 'close')
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`)
 values ('Cook Out Northside', 33.785686, -84.4098867, 'American', TRUE, FALSE, b'1111111', '10:30', '4:00');
 
-insert into restaurants ('name', 'lat', 'long', 'cuisine', 'indoor','outdoor', 'days', 'open', 'close')
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`)
 values ('Cook Out Ponce', 33.774611, -84.3713277, 'American', TRUE, FALSE, b'1111111', '10:30', '4:00');
 
-insert into restaurants ('name', 'lat', 'long', 'cuisine', 'indoor','outdoor', 'days', 'open', 'close')
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`)
 values ('Cook Out Moreland', 33.7431083, -84.3669767, 'American', TRUE, FALSE, b'1111111', '10:30', '4:00');
 
-insert into restaurants ('name', 'lat', 'long', 'cuisine', 'indoor','outdoor', 'days', 'open', 'close')
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`)
 values ('Sidebar', 33.757033, -84.3921116, 'American', TRUE, FALSE, b'1111111', '11:00', '24:00');
 
-insert into restaurants ('name', 'lat', 'long', 'cuisine', 'indoor','outdoor', 'days', 'open', 'close')
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`)
 values ('Benihana', 33.7604083, -84.3885077, 'Japanese', TRUE, FALSE, b'1111111', '11:00', '21:30');
 
-insert into restaurants ('name', 'lat', 'long', 'cuisine', 'indoor','outdoor', 'days', 'open', 'close')
+insert into restaurants (`name`, `lat`, `long`, `cuisine`, `indoor`,`outdoor`, `days`, `open`, `close`)
 values ('Sun Dial', 33.7593505,-84.3907603, 'Fine Dining', TRUE, FALSE, b'1111111', '18:00', '22:00');
